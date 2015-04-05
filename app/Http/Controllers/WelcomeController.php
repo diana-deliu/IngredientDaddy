239c401e756276aa99a354b8639b86e220ffc4db
ahlexander <alex@devmotion.ro>
2015-04-05 14:51:05 +0300

    Went back to default laravel auth

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -30,7 +30,7 @@ class WelcomeController extends Controller {
 	 */
 	public function index()
 	{
-		return 'hello';
+		return view('pages.index');
 	}
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', 'MainController@index');
+Route::get('/', 'WelcomeController@index');
+Route::get('home', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
